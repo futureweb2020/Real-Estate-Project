b1cb279476b08aa9064b794f74ac693e625582b3
futureweb2020 <futureweb2020@yandex.com>
2017-12-01 06:41:56 +0800

    feat: add some blade templates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,90 +1,72 @@
-@extends('layouts.blank')
+@extends('layouts.main')
+
+@section('pagetitle')
+Login
+@endsection
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3">
-			<div class="jarviswidget" id="wid-id-3" data-widget-editbutton="false" data-widget-custombutton="false">
-				<!-- widget options:
-					usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="false">
-					
-					data-widget-colorbutton="false"	
-					data-widget-editbutton="false"
-					data-widget-togglebutton="false"
-					data-widget-deletebutton="false"
-					data-widget-fullscreenbutton="false"
-					data-widget-custombutton="false"
-					data-widget-collapsed="true" 
-					data-widget-sortable="false"
-					
-				-->
-				<header>
-					<span class="widget-icon"> <i class="fa fa-edit"></i> </span>
-					<h2>Login </h2>				
-					
-				</header>
+<div class="col-md-4 col-md-offset-3">
+	<div class="jarviswidget" id="wid-id-3" data-widget-editbutton="false" data-widget-custombutton="false">
+		<header>
+			<span class="widget-icon"> <i class="fa fa-edit"></i> </span>
+			<h2>Login </h2>				
+		</header>
 
-				<!-- widget div-->
-				<div>
-					
-					<!-- widget edit box -->
-					<div class="jarviswidget-editbox">
-						<!-- This area used as dropdown edit box -->
-						
-					</div>
-					<!-- end widget edit box -->
-					
-					<!-- widget content -->
-					<div class="widget-body no-padding">
-						
-						<form id="login-form" method="post" class="smart-form" novalidate="novalidate">
-							<fieldset>
-								<div class="row">
-									<section class="col col-md-12">
-										<label class="input"> <i class="icon-append fa fa-envelope-o"></i>
-											<input type="email" name="email" placeholder="E-mail">
-										</label>
-										<i></i> 
-									</section>
-								</div>
-
-								<div class="row">
-									<section class="col col-md-12">
-										<label class="input"> <i class="icon-append fa fa-lock"></i>
-											<input type="password" name="password" placeholder="********">
-										</label>
-										<i></i> 
-									</section>
-								</div>
-							</fieldset>
-
-							<fieldset>
-								<div class="row">
-									<section class="col-md-6">
-										<label class="checkbox">
-											<input type="checkbox" name="remember"> Remember Me
-										</label>
-									</section>
-								</div>
-							</fieldset>
-
-							<footer>
-								<button type="submit" class="btn btn-primary">
-									Login!
-								</button>
-							</footer>
-						</form>
-
-					</div>
-					<!-- end widget content -->
-					
-				</div>
-				<!-- end widget div -->
-
+		<!-- widget div-->
+		<div>
+			<!-- widget edit box -->
+			<div class="jarviswidget-editbox">
+				<!-- This area used as dropdown edit box -->
 			</div>
-			<!-- end widget -->
+			<!-- end widget edit box -->
+			
+			<!-- widget content -->
+			<div class="widget-body no-padding">
+				<form id="login-form" method="post" class="smart-form" novalidate="novalidate">
+					<fieldset>
+						<div class="row">
+							<section class="col col-md-12">
+								<label class="input"> <i class="icon-append fa fa-envelope-o"></i>
+									<input type="email" name="email" placeholder="E-mail">
+								</label>
+								<i></i> 
+							</section>
+						</div>
+
+						<div class="row">
+							<section class="col col-md-12">
+								<label class="input"> <i class="icon-append fa fa-lock"></i>
+									<input type="password" name="password" placeholder="********">
+								</label>
+								<i></i> 
+							</section>
+						</div>
+					</fieldset>
+
+					<!-- <fieldset>
+						<div class="row">
+							<section class="col-md-6">
+								<label class="checkbox">
+									<input type="checkbox" name="remember"> Remember Me
+								</label>
+							</section>
+						</div>
+					</fieldset> -->
+
+					<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+					<footer>
+						<button type="submit" class="btn btn-primary">
+							Login!
+						</button>
+					</footer>
+				</form>
+			</div>
+			<!-- end widget content -->					
 		</div>
+		<!-- end widget div -->
 	</div>
+	<!-- end widget -->
 </div>
 @endsection
 
---
+++ resources/views/dashboards/landlord.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('pagetitle')
+Dashboard
+@endsection
+
+@section('pagesubtitle')
+
+@endsection
+
+@section('content')
+
+@endsection
